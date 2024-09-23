ffa2ca34e08bfdd12b9cc1347387c21081de88ff
caiof_lima <dev.caiolima@gmail.com>
2024-09-22 22:56:09 -0300

    fix settings.py by removing sqlite3 default

--- extensionista/settings.py
+++ extensionista/settings.py
@@ -65,24 +65,16 @@ WSGI_APPLICATION = 'extensionista.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         "HOST": env('DATABASE_HOST'),
-#         "PORT": env('DATABASE_PORT'),
-#         'NAME': env('DATABASE_NAME'),
-#         'USER': env('DATABASE_USER'),
-#         'PASSWORD': env('DATABASE_PASS'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "HOST": env('DATABASE_HOST'),
+        "PORT": env('DATABASE_PORT'),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
