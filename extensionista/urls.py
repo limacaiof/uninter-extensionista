e1ddb6e7bb2396f191017f512a5a45b38a2bebe0
caiof_lima <dev.caiolima@gmail.com>
2024-09-26 21:59:23 -0300

    base html templates and urls

--- extensionista/urls.py
+++ extensionista/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', view=views.home),
+    path('', view=views.home, name='home'),
+    path('campanhas', view=views.campaigns, name='campaigns'),
+    path('sobre', view=views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
